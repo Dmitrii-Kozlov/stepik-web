cf3ace8429141f0c8aa987dab2c26acfe27cd8a3
Dmitrii-Kozlov <demetra_n@front.ru>
2021-11-22 17:22:39 +0300

    create ask project, create qa app adjust nginx and gunicorn

--- init.sh
+++ init.sh
@@ -1,4 +1,6 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/nginx.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-gunicorn -b 0.0.0.0:8080 hello:wsgi_application &
+#gunicorn -b 0.0.0.0:8000 hello:wsgi_application &
+
+#gunicorn -b 0.0.0.0:8000 ask.wsgi
